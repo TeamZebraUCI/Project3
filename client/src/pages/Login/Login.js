bd012556024fac455bdf200100cee93766db8abe
JohnnyC <j0hnnywchiang@gmail.com>
2018-07-17 12:57:02 -0700

    save progress

--- client/src/pages/Login/Login.js
+++ client/src/pages/Login/Login.js
@@ -1,10 +1,14 @@
-import React,{Component} from "react";
+import React,{Component, Fragment} from "react";
+import LoginComp from '../../components/Login/Login'
 
 class Login extends Component{
 
     render(){
         return (
+            <Fragment>
             <h1>THIS IS THE LOGIN PAGE</h1>
+            <LoginComp />
+            </Fragment>
         );
     }
 };
