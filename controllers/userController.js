469867ce678fe07e16d26f2d7bcc91bc039925b7
Lunad3 <123.daniel.luna@gmail.com>
2018-07-17 13:51:03 -0700

    server works, api routes for db work (tested on user) controller grabs req correctly models processed removed yarn start error msgs

--- controllers/userController.js
+++ controllers/userController.js
@@ -2,7 +2,7 @@ const db = require("../models");
 module.exports = {
     create: (req, res)=>{
         console.log("CREATE USER:");
-        console.log(req);
+        console.log(req.body);
         const user = {
             username: req.body.username,
             password: req.body.password,
@@ -12,16 +12,16 @@ module.exports = {
     update: (req, res)=>{
         console.log("UPDATE USER:");
         console.log("--DO NOTHING");
-        console.log(req);
+        console.log(req.body);
     },
     remove: (req, res)=>{
         console.log("DELETE USER:");
         console.log("--DO NOTHING");
-        console.log(req);
+        console.log(req.body);
     },
     find: (req, res)=>{ 
         console.log("FIND USER");
         console.log("--DO NOTHING");
-        console.log(req);
+        console.log(req.body);
     }
 };
