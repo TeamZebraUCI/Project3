a076e185f2adfff04a229f124de84e183a27e66d
JohnnyC <j0hnnywchiang@gmail.com>
2018-07-17 20:13:03 -0700

    notes with material-ui

--- client/src/components/Notes/Notes.js
+++ client/src/components/Notes/Notes.js
@@ -1,5 +1,10 @@
 import React, { Component } from 'react'
-import './Notes.css'
+import TextField from '@material-ui/core/TextField'
+import Paper from '@material-ui/core/Paper'
+import Button from '@material-ui/core/Button'
+import { List, ListItem, ListItemSecondaryAction, IconButton, ListItemText } from '@material-ui/core'
+import DeleteIcon from '@material-ui/icons/Delete'
+import Typography from '@material-ui/core/Typography'
 
 export default class Notes extends Component {
     constructor() {
@@ -30,25 +35,37 @@ export default class Notes extends Component {
     render() {
         let pinNote = this.state.notes.map((e, i) => {
             return (
-                <li key={i}>{e}<button onClick={() => this.deleteNote(i)}>x</button></li>
+                <ListItem key={i}><ListItemText>{e}</ListItemText>
+                    <ListItemSecondaryAction>
+                        <IconButton aria-label="Delete" onClick={() => this.deleteNote(i)} >
+                            <DeleteIcon />
+                        </IconButton>
+                    </ListItemSecondaryAction>
+                </ListItem>
             );
         });
         return (
-            <div className='notesArea'>
-                {this.state.notes.length === 0 ? "No notes yet!" : <ul>{pinNote}</ul>}
-                <br />
+            <Paper style={{ width: "25%", marginTop: "3em" }}>
+                <Typography variant="title" style={{ textAlign: 'center' }}>
+                    Pinned Notes
+            </Typography>
+                <Paper style={{ paddingBottom: '20px' }}>
+                    <List>
+                        {this.state.notes.length === 0 ? <ListItem>Add a note!</ListItem> : pinNote}
+                    </List>
+                </Paper>
                 <form>
-                    <div className='notes'>
-                        <input
-                            className='textArea'
-                            placeholder='Create a new note!'
-                            value={this.state.currentNote}
-                            onChange={this.onInputChange}
-                            type="text" />
-                        <button onClick={this.onClick}>Add</button>
-                    </div>
+                    <TextField
+                        name="AddNote"
+                        value={this.state.currentNote}
+                        onChange={this.onInputChange}
+                    />
+                    <Button
+                        color="primary"
+                        type="submit"
+                        onClick={this.onClick}>Add Note</Button>
                 </form>
-            </div>
+            </Paper>
         )
     }
 }
