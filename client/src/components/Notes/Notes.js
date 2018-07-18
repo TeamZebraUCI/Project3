97051fa114c7bfad624a8d8e97d64eaf2c37157e
JohnnyC <j0hnnywchiang@gmail.com>
2018-07-18 10:07:03 -0700

    material ui still not working

--- client/src/components/Notes/Notes.js
+++ client/src/components/Notes/Notes.js
@@ -1,10 +1,5 @@
 import React, { Component } from 'react'
-import TextField from '@material-ui/core/TextField'
-import Paper from '@material-ui/core/Paper'
-import Button from '@material-ui/core/Button'
-import { List, ListItem, ListItemSecondaryAction, IconButton, ListItemText } from '@material-ui/core'
-import DeleteIcon from '@material-ui/icons/Delete'
-import Typography from '@material-ui/core/Typography'
+import './Notes.css'
 
 export default class Notes extends Component {
     constructor() {
@@ -22,50 +17,38 @@ export default class Notes extends Component {
     onClick = e => {
         e.preventDefault();
         if (this.state.currentNote) {
-            let newNote = this.state.notes.slice();
-            newNote.push(this.state.currentNote);
-            this.setState({ notes: newNote, currentNote: '' });
+        let newNote = this.state.notes.slice();
+        newNote.push(this.state.currentNote);
+        this.setState({ notes: newNote, currentNote: '' });
         }
     }
     deleteNote = i => {
         let removeNote = this.state.notes.slice();
-        removeNote.splice(i, 1);
+        removeNote.splice(i , 1);
         this.setState({ notes: removeNote })
     }
     render() {
         let pinNote = this.state.notes.map((e, i) => {
             return (
-                <ListItem key={i}><ListItemText>{e}</ListItemText>
-                    <ListItemSecondaryAction>
-                        <IconButton aria-label="Delete" onClick={() => this.deleteNote(i)} >
-                            <DeleteIcon />
-                        </IconButton>
-                    </ListItemSecondaryAction>
-                </ListItem>
+                <li key={i}>{e}<button onClick={() => this.deleteNote(i)}>x</button></li>
             );
         });
         return (
-            <Paper style={{ width: "25%", marginTop: "3em" }}>
-                <Typography variant="title" style={{ textAlign: 'center' }}>
-                    Pinned Notes
-            </Typography>
-                <Paper style={{ paddingBottom: '20px' }}>
-                    <List>
-                        {this.state.notes.length === 0 ? <ListItem>Add a note!</ListItem> : pinNote}
-                    </List>
-                </Paper>
+            <div className='notesArea'>
+                {this.state.notes.length === 0 ? "No notes yet!" : <ul>{pinNote}</ul>}
+                <br />
                 <form>
-                    <TextField
-                        name="AddNote"
-                        value={this.state.currentNote}
-                        onChange={this.onInputChange}
-                    />
-                    <Button
-                        color="primary"
-                        type="submit"
-                        onClick={this.onClick}>Add Note</Button>
+                    <div className='notes'>
+                        <input
+                            className='textArea'
+                            placeholder='Create a new note!'
+                            value={this.state.currentNote}
+                            onChange={this.onInputChange}
+                            type="text" />
+                        <button onClick={this.onClick}>Add</button>
+                    </div>
                 </form>
-            </Paper>
+            </div>
         )
     }
 }
