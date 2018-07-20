741fa5dc22a4b34277c2cc2211f0efa313c168bc
Zachary Guns <zacharyguns@yahoo.com>
2018-07-19 23:08:40 -0700

    bout to throw my laptop out the window

--- client/src/components/MyChart/index.js
+++ client/src/components/MyChart/index.js
@@ -1,1 +1,2 @@
-export { default } from "./myChart";
+export { default } from "./myChart.js";
+// export * from "./myChart.js";
--- client/src/pages/Home/Home.js
+++ client/src/pages/Home/Home.js
@@ -1,33 +1,51 @@
-import React,{Component} from "react";
+import React, { Component } from "react";
 import "./Home.css";
-import {Helmet} from "react-helmet";
+import { Helmet } from "react-helmet";
 import Page from "../../components/MaterializePage";
+import MyChart from "../../components/MyChart/myChart";
 
-class Home extends Component{
+class Home extends Component {
 
-    render(){
+    render() {
         return (
             <Page
                 style={
                     {
-                    "background-color":"#9b179b",//<------ THEME COLOR
+                        "background-color": "#9b179b",//<------ THEME COLOR
                     }
                 }
                 text="P3"//<---- LOGO TEXT
                 promptLogin={true}
-                username = {this.props.username}
+                username={this.props.username}
             >
                 <Helmet>
                     <title>Home</title>
                 </Helmet>
                 <h1>THIS IS THE HOME PAGE</h1>
                 <div class="wrapper">
-                    <canvas id="canvas" width="1600"  height="900"></canvas>
+                    <canvas id="myChart" width="1600" height="900"></canvas>
                 </div>
+                <button id="nike">
+                    Nike
+                </button>
+                <button id="lexus">
+                    Lexus
+                </button>
+                <button id="apple">
+                    Apple
+                </button>
+                <button id="addYear">
+                    Add time
+                </button>
+                <button id="removeYear">
+                    Remove time
+                </button>
+                <button id="empty">
+                    Remove Dataset
+                </button>
             </Page>
-        );
-    }
-};
-
-// import {myChart} from "./chart.js";
+                    );
+                }
+            };
+            
 export default Home;
