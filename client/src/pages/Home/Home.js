86d75ee77edbb2c8ab9eeded238248ca00582ca0
LunaPC <lunad3@uci.edu>
2018-07-19 21:53:42 -0700

    fixed naming issue for TickerList

--- client/src/pages/Home/Home.js
+++ client/src/pages/Home/Home.js
@@ -2,44 +2,15 @@ import React,{Component} from "react";
 import "./Home.css";
 import {Helmet} from "react-helmet";
 import Page from "../../components/MaterializePage";
-import Note from "../../components/Notes";
-import List from "../../components/TickerList";
-import StockAPI from "../../utils/API";
+import TickerList from "../../components/TickerList";
 
 
 class Home extends Component{
 
-<<<<<<< HEAD
     selectTicker = (event)=>{
         console.log(event);
     };
 
-=======
-    state={
-        tickerList:[]
-    }
-
-    searchTicker = query => {
-        console.log(query)
-        console.log("USING SEARCHTICKER FROM HOMEPAGE");
-        if (this.state.query){
-            StockAPI.searchSymbol(this.state.query.trim()).then(res=>{
-                let newTickerList = this.state.tickerList;
-                const newTicker = {
-                    ticker: this.state.query,
-                    logoURL: res.data.url
-                }
-                newTickerList.push(newTicker);
-                this.setState({ tickerList: newTickerList, query: "" });
-                console.log(res);
-            }).catch(error=>{
-                console.log("API::SearchSymbol::FAIL");
-            });
-        }
-    };
-
-
->>>>>>> dd805e6d3c3629e2de1a0a060e150238f7ffd46b
     render(){
         return (
             <Page
@@ -56,23 +27,11 @@ class Home extends Component{
                     <title>Home</title>
                 </Helmet>
                 <div className="Home row">
-<<<<<<< HEAD
                     <div className="col s3 mycol1">
                     <TickerList
                         selectHandler={this.selectTicker}
                     /></div>
                     <div className="col s9 mycol2">asdf</div>
-=======
-                    <div className="col s3 mycol1"><List
-                        buttonHandler={this.searchTicker}
-                        query = {this.state.query}
-                        inputChangeHandler={this.onTickerInputChange}                    
-                    /></div>
-                    <div className="col s9 mycol2">
-                        <div className="row">GRAPH HERE</div>
-                        <div className="row"><Note/></div>
-                    </div>
->>>>>>> dd805e6d3c3629e2de1a0a060e150238f7ffd46b
                 </div>
             </Page>
         );
